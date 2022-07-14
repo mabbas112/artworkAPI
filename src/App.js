616b687fba6eacdc3e7e1faa229fff5874842f08
mabbas112 <mabbasctn112@gmail.com>
2022-07-14 18:39:34 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import Objects from './Components/Objects/objects';
+import Object from './Components/Object/object';
+import { ObjectCtxProvider } from './store/objects-ctx';
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ObjectCtxProvider>
+      <Object/>
+    </ObjectCtxProvider>
   );
 }
 
